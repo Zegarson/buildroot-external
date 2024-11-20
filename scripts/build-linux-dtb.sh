b859cd58d8c066f3a1e706fbb0a6cc0d48b93002
Franciszek Łopuszański <franopusz2006@gmail.com>
2024-11-21 00:22:51 +0100

    Add helper scripts

---
+++ scripts/build-linux-dtb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+LINUX_CUSTOM_DTS="../board/st/zegarson/dts/kernel"
+LINUX_SRC="../../buildroot/build/linux-custom"
+OUTPUT_DIR="../out"
+DTS_FILE="stm32mp157d-zegarson.dts"
+DTB_FILE="stm32mp157d-zegarson.dtb"
+
+# Copy custom DTS files
+cp $LINUX_CUSTOM_DTS/* "$LINUX_SRC/arch/arm/boot/dts/"
+
+# Preprocess and compile DTS
+cd $LINUX_SRC || exit
+cpp -nostdinc -I include -I arch -undef -x assembler-with-cpp "arch/arm/boot/dts/$DTS_FILE" > "$DTS_FILE.preprocessed"
+dtc -I dts -O dtb -o "$DTB_FILE" "$DTS_FILE.preprocessed"
+
+cd $CURRENT_DIR
+mv $LINUX_SRC/*.dtb $OUTPUT_DIR
+rm $LINUX_SRC/*.preprocessed
